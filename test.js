8bd3236194991707001324ca8539c5429cb6f1e9
baukh789 <182209508@qq.com>
2018-09-26 10:53:28 +0800

    引入https

--- test.js
+++ test.js
@@ -1,8 +1,19 @@
 var express = require('express');
+var https = require('https');
+var fs = require('fs');
 var app = express();
 
 // 静态文件
 app.use(express.static(__dirname));
+// app.listen(8003, function () {
+//     console.log('Server running at :' + 8003);
+// });
+var privateKey  = fs.readFileSync('./cert/private.key', 'utf8'),
+    certificate = fs.readFileSync('./cert/file.crt', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
+
+https.createServer(credentials, app).listen(4003);
+
 app.listen(8003, function () {
     console.log('Server running at :' + 8003);
 });
